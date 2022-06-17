cd06b72171e2462e69ee0529f8988f27794d4666
Hishamabboud <56691848+Hishamabboud@users.noreply.github.com>
2022-06-17 16:29:31 +0200

    Delete Auth directory

--- Auth/AuthRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ecommerence.Auth
-{
-    public class AuthRequestDto
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string RefreshToken { get; set; }
-    }
-}
--- Auth/RefreshTokenData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ecommerence.Auth
-{
-    public class RefreshTokenData
-    {
-        public string RefreshToken { get; set; }
-        public string Email { get; set; }
-    }
-}
